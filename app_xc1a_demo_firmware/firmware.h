dae6d15215a2662713e4b943aa71385d37b6e756
Dave Lacey <david.lacey@xmos.com>
2011-07-22 12:38:01 +0100

    Updated copyright

--- app_xc1a_demo_firmware/firmware.h
+++ app_xc1a_demo_firmware/firmware.h
@@ -1,22 +1,8 @@
-/**
- * Module:  app_xc1a_firmware
- * Version: 1v3
- * Build:   0d899968f20a382e3779bfcd0446c7abe32cf649
- * File:    firmware.h
- *
- * The copyrights, all other intellectual and industrial 
- * property rights are retained by XMOS and/or its licensors. 
- * Terms and conditions covering the use of this code can
- * be found in the Xmos End User License Agreement.
- *
- * Copyright XMOS Ltd 2010
- *
- * In the case where this code is a modification of existing code
- * under a separate license, the separate license terms are shown
- * below. The modifications to the code are still covered by the 
- * copyright notice above.
- *
- **/                                   
+// Copyright (c) 2011, XMOS Ltd., All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 
 #ifndef _FIRMWARE_H_
 #define _FIRMWARE_H_
